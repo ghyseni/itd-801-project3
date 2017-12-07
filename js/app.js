8ade7ab6466382ea30d89a9646a4c744f850e2ef
Grese Hyseni <ghyseni@hawk.iit.edu>
2017-12-07 14:17:44 -0600

    Control toggle navigation using css responsive feature 'expanded-nav

--- js/app.js
+++ js/app.js
@@ -1,14 +1,36 @@
+$('html').addClass('js').removeClass('no-js');
+$('.cssgrid').removeClass('.cssgrid');
+$('.flexbox').removeClass('.flexbox');
+
 $(document).ready(function() {
-
+  toggleMenuButton();
   /* Open/close navigation on navigation button click */
-  $('#show-nav').click(function() {
-    if (!$(this).hasClass('close')) {
-      $(this).addClass('close');
-      $('#nav').addClass('opened');
-    } else {
-      $(this).removeClass('close');
-      $('#nav').removeClass('opened');
-    }
+  $('#show-nav').on('click', function() {
+    $('#show-nav').toggleClass('close');
+    $('#nav').toggleClass('opened');
   });
-
 });
+
+$(window).resize(function() {
+  toggleMenuButton();
+});
+
+function toggleMenuButton() {
+  if (responsiveFeature('expanded-nav')) {
+    if ($('#show-nav').length === 0) {
+      $('#header').append('<button id="show-nav" class=""><span class="button-line"></span></button>');
+    }
+  } else {
+    $('#show-nav').remove();
+  }
+}
+
+function responsiveFeature(feature) {
+  var body = document.body;
+  var size = window.getComputedStyle(body, ':after').getPropertyValue('content');
+  var has_feature = true;
+  if (size.indexOf(feature) === -1) {
+    has_feature = false;
+  }
+  return has_feature;
+}
